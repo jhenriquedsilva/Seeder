bbb4290ccc309767af759d3e938d676acd7c80db
jhenriquedsilva <josehs555@gmail.com>
2022-05-28 14:44:10 -0300

    feat: improve exception handling in auth service

--- lib/network/authentication_service.dart
+++ lib/network/authentication_service.dart
@@ -8,7 +8,8 @@ import '../models/user.dart';
 class AuthenticationService {
   final _baseUrl = "https://learning-data-sync-mobile.herokuapp.com/datasync/api";
 
-  Future<User?> login(String email) async  {
+  Future<User> login(String email) async  {
+
     final url = Uri.parse("$_baseUrl/user/auth");
     final response = await post(
       url,
@@ -21,32 +22,33 @@ class AuthenticationService {
       }
     );
 
-    if (response.statusCode == 404) {
+    if (response.statusCode >= 200 && response.statusCode < 300){
+      return User.fromJson(json.decode(response.body));
+
+    } else if (response.statusCode == 404) {
       throw EmailDoesNotExistException();
+
     } else if (response.statusCode >= 400 && response.statusCode < 500) {
       throw Exception("Dados inválidos");
+
     } else if (response.statusCode >= 500 && response.statusCode < 600) {
       throw Exception('Erro no servidor');
-    } else if (response.statusCode >= 200 && response.statusCode < 300){
-      return parseLoginResponseToUser(json.decode(response.body));
+
+    } else {
+      throw Exception(response.body);
     }
 
-    return null;
   }
 
-  User parseLoginResponseToUser(Map<String, dynamic> loginResponse) {
-    return User(loginResponse["id"] as String);
-  }
-
-  Future<String?> signup(String fullName, String email) async {
+  Future<User> signup(String fullName, String email) async {
 
     final url = Uri.parse("$_baseUrl/user");
     final id = const Uuid().v4().toString();
-    final body = json.encode({
+    final userData = {
       "id": id,
       "fullName": fullName,
       "email": email,
-    });
+    };
 
     final headers = {
       "content-type": "application/json",
@@ -58,22 +60,28 @@ class AuthenticationService {
       final response = await post(
         url,
         headers: headers,
-        body: body,
+        body: json.encode(userData),
       );
 
-      if (response.statusCode == 400) {
+      if (response.statusCode >= 200 && response.statusCode < 300) {
+        return User.fromJson(userData);
+
+      } else if (response.statusCode == 400) {
         throw EmailNotValidException();
+
       } else if (response.statusCode == 409) {
         throw EmailAlreadyExistException();
+
       } else if (response.statusCode >= 400 && response.statusCode < 500) {
         throw Exception("Dados inválidos");
+
       } else if (response.statusCode >= 500 && response.statusCode < 600) {
         throw Exception('Erro no servidor');
-      } else if (response.statusCode >= 200 && response.statusCode < 300) {
-        return id;
-      }
 
-      return null;
+      } else {
+        throw Exception(response.body);
+
+      }
 
     } catch (error) {
       rethrow;
