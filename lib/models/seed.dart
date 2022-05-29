b3c189df7c0f50f57e7cd7125acafd4c24a17552
jhenriquedsilva <josehs555@gmail.com>
2022-05-29 16:25:30 -0300

    feat: add application seed

---
+++ lib/models/seed.dart
@@ -0,0 +1,15 @@
+class Seed {
+
+  const Seed({
+    required this.name,
+    required this.manufacturer,
+    required this.manufacturedAt,
+    required this.expiresIn,
+  });
+
+  final String name;
+  final String manufacturer;
+  final String manufacturedAt;
+  final String expiresIn;
+
+}
