fc29e0df8b728314a2739a1809a69c85af021d5e
jhenriquedsilva <josehs555@gmail.com>
2022-05-29 16:25:01 -0300

    feat: add network seed model

---
+++ lib/models/network_seed.dart
@@ -0,0 +1,19 @@
+class NetworkSeed {
+
+  const NetworkSeed({
+    required this.id,
+    required this.name,
+    required this.manufacturer,
+    required this.manufacturedAt,
+    required this.expiresIn,
+    required this.userId,
+  });
+
+  final String id;
+  final String name;
+  final String manufacturer;
+  final String manufacturedAt;
+  final String expiresIn;
+  final String userId;
+
+}
