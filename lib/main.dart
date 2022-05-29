e2f388c5412cc0dba96ee9fa60bcb34ed582bdc9
jhenriquedsilva <josehs555@gmail.com>
2022-05-29 16:17:29 -0300

    feat: add seed provider

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:seed/providers/auth_provider.dart';
+import 'package:seed/providers/seed_provider.dart';
 import 'package:seed/ui/screens/sign_in_sign_up_screen.dart';
 import 'package:seed/ui/screens/seed_screen.dart';
 
@@ -22,32 +23,30 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [ChangeNotifierProvider(create: (_) => AuthProvider())],
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => SeedProvider())
+      ],
       child: Consumer<AuthProvider>(
         builder: (_, authProvider, __) => MaterialApp(
           title: 'Seeder App',
-
           home: FutureBuilder<bool>(
             future: authProvider.isAuthenticated(),
             builder: (context, AsyncSnapshot<bool> snapshot) {
-
               if (snapshot.connectionState == ConnectionState.waiting) {
                 return const Center(
                   // TODO Create a splash screen
                   child: CircularProgressIndicator(),
                 );
-
               } else {
                 final isAuthenticated = snapshot.data as bool;
 
                 if (isAuthenticated) {
-                  return SeedScreen();
+                  return const SeedsScreen();
                 } else {
                   return const SingInSignUpScreen();
                 }
-
               }
-
             },
           ),
         ),
