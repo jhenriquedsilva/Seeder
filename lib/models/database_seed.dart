5adef65c8041e4cd3808c8c360f2e36478ca8065
jhenriquedsilva <josehs555@gmail.com>
2022-06-03 11:12:36 -0300

    feat: add fromJson method to simplify object creation when querying data from database

--- lib/models/database_seed.dart
+++ lib/models/database_seed.dart
@@ -5,6 +5,7 @@ class DatabaseSeed {
       required this.manufacturer,
       required this.manufacturedAt,
       required this.expiresIn,
+      required this.createdAt,
       required this.synchronized});
 
   final String id;
@@ -12,7 +13,20 @@ class DatabaseSeed {
   final String manufacturer;
   final String manufacturedAt;
   final String expiresIn;
+  final String createdAt;
   final int synchronized;
+
+  factory DatabaseSeed.fromMap(Map<String, dynamic> map) {
+    return DatabaseSeed(
+      id: map['id'],
+      name: map['name'],
+      manufacturer: map['manufacturer'],
+      manufacturedAt: map['manufacturedAt'],
+      expiresIn: map['expiresIn'],
+      createdAt: map['createdAt'],
+      synchronized: map['synchronized'],
+    );
+  }
 
   Map<String, dynamic> toMap() {
     return {
@@ -21,18 +35,20 @@ class DatabaseSeed {
       'manufacturer': manufacturer,
       'manufacturedAt': manufacturedAt,
       'expiresIn': expiresIn,
+      'createdAt': createdAt,
       'synchronized': synchronized,
     };
   }
 
   @override
   String toString() {
-    return """DatabaseSeed{
+    return '''DatabaseSeed{
     id: $id, 
     name: $name, 
     manufacture: $manufacturer, 
     manufacturedAt: $manufacturedAt, 
-    expiresIn: $expiresIn, 
-    synchronized: $synchronized}""";
+    expiresIn: $expiresIn,
+    createdAt: $createdAt, 
+    synchronized: $synchronized}''';
   }
 }
