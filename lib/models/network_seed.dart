43e6d7c3669ddb7b0e8d4611e07686094e47b41f
jhenriquedsilva <josehs555@gmail.com>
2022-06-03 11:36:50 -0300

    feat: add toJson, fromJson, toString methods

--- lib/models/network_seed.dart
+++ lib/models/network_seed.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class NetworkSeed {
   const NetworkSeed({
     required this.id,
@@ -13,6 +15,39 @@ class NetworkSeed {
   final String manufacturer;
   final String manufacturedAt;
   final String expiresIn;
-  final String userId;
+  final String createdAt;
 
+  String toJson(String userId) {
+    return json.encode({
+      'id': id,
+      'name': name,
+      'manufacturer': manufacturer,
+      'manufacturedAt': manufacturedAt,
+      'expiresIn': expiresIn,
+      'createdAt': createdAt,
+      'userId': userId,
+    });
+  }
+
+  factory NetworkSeed.fromJson(Map<String, dynamic> json) {
+    return NetworkSeed(
+      id: json['id'],
+      name: json['name'],
+      manufacturer: json['manufacturer'],
+      manufacturedAt: json['manufacturedAt'],
+      expiresIn: json['expiresIn'],
+      createdAt: json['createdAt'],
+    );
+  }
+
+  @override
+  String toString() {
+    return '''NetworkSeed{
+    id: $id, 
+    name: $name, 
+    manufacture: $manufacturer, 
+    manufacturedAt: $manufacturedAt, 
+    expiresIn: $expiresIn,
+    createdAt: $createdAt}''';
+  }
 }
