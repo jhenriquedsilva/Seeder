bcbe29d238a7252deb8348cd0c1dc133664edd21
jhenriquedsilva <josehs555@gmail.com>
2022-06-05 19:11:37 -0300

    feature: improve seed screen ui

--- lib/ui/screens/seed_screen.dart
+++ lib/ui/screens/seed_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:seed/providers/auth_provider.dart';
 import 'package:seed/providers/seed_provider.dart';
@@ -35,187 +36,42 @@ class _SeedsScreenState extends State<SeedsScreen> {
           'Sementes',
           style: Theme.of(context).textTheme.headline4,
         ),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.sync),
-            onPressed: () async {
-              try {
-                await Provider.of<SeedProvider>(context, listen: false)
-                    .synchronize();
-                showSnackBar(context, 'Sementes sincronizadas com sucesso');
-              } catch (error) {
-                showSnackBar(context, error.toString());
-              }
-            },
-          ),
-          IconButton(
-              onPressed: () async {
-                final areThereNonSynchronizedSeeds =
-                    await Provider.of<SeedProvider>(context, listen: false)
-                        .areThereAnyNonSynchronized();
-                if (areThereNonSynchronizedSeeds) {
-                  final isLoggingOut = await showDialog<bool>(
-                    context: context,
-                    builder: (_) => AlertDialog(
-                      title: const Text('Sementes não sincronizadas'),
-                      content: const Text(
-                          'Se você sair agora, perderá as sementes não sincronizadas'),
-                      actions: [
-                        TextButton(
-                            onPressed: () {
-                              Navigator.of(context).pop(true);
-                            },
-                            child: const Text('Sair')),
-                        TextButton(
-                            onPressed: () {
-                              Navigator.of(context).pop(false);
-                            },
-                            child: const Text('Sincronizar'))
-                      ],
-                    ),
-                  );
-
-                  // TODO Implement search functionality
-                  // TODO Consider the case when there are no seeds
-                  if (isLoggingOut != null && isLoggingOut) {
-                    await Provider.of<SeedProvider>(context, listen: false)
-                        .clear();
-                    await Provider.of<AuthProvider>(context, listen: false)
-                        .logout();
-                  }
-                } else {
-                  await Provider.of<AuthProvider>(context, listen: false)
-                      .logout();
-                }
-              },
-              icon: const Icon(Icons.logout))
-        ],
+        actions: _buildAppBarIconButtons(),
       ),
-      body: SafeArea(
-        child: SingleChildScrollView(
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).requestFocus(FocusNode());
+        },
+        child: SafeArea(
           child: Column(
             children: [
-              Padding(
-                padding: const EdgeInsets.all(16),
-                child: TextField(
-                  decoration: InputDecoration(
-                    enabledBorder: const OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(8)),
-                        borderSide: BorderSide(color: Colors.green)),
-                    focusedBorder: const OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(8)),
-                        borderSide:
-                            BorderSide(color: Colors.green, width: 2.0)),
-                    hintText: 'Buscar sementes...',
-                    hintStyle: Theme.of(context).textTheme.labelMedium!.copyWith(color: Colors.black)
-                  ),
-                  onChanged: (query) {
-                    Provider.of<SeedProvider>(context, listen: false)
-                        .searchSeeds(query);
-                  },
-                ),
-              ),
+              _buildSearchBox(),
               FutureBuilder(
                 future: Provider.of<SeedProvider>(context, listen: false)
                     .cacheSeeds(),
                 builder: (_, snapshot) {
                   if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(
-                      child: CircularProgressIndicator(),
+                    return Center(
+                      child: CircularProgressIndicator(
+                        color: Theme.of(context).primaryColor,
+                      ),
                     );
 
                     // If a time out exception is thrown
                   } else if (snapshot.hasError) {
-                    return Center(
-                      child: SingleChildScrollView(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Center(
-                              child: Text(
-                                snapshot.error.toString(),
-                                textAlign: TextAlign.center,
-                                style: Theme.of(context).textTheme.labelMedium!.copyWith(color: Colors.black),
-                              ),
-                            ),
-                            const SizedBox(height: 24,),
-                            ElevatedButton(
-                                onPressed: () {
-                                  setState(() {});
-                                },
-                                child: Text(
-                                  'Recarregar',
-                                  style: Theme.of(context).textTheme.button!.copyWith(color: Colors.white),
-                                ),
-                                style: ElevatedButton.styleFrom(
-                                  primary: Theme.of(context).primaryColor,
-                                  padding: const EdgeInsets.all(16),
-                                  fixedSize: Size(MediaQuery.of(context).size.width * 0.7, 60),
-                                  elevation: 16,
-                                  shadowColor: Colors.green,
-                                  // side: const BorderSide(
-                                  //   color: Colors.black,
-                                  //   width: 2,
-                                  // ),
-                                  shape: const StadiumBorder(),
-                                )
-                              ),
-                          ],
-                        ),
-                      ),
-                    );
+                    return _buildTimeOutExceptionWarning(snapshot);
                   } else {
                     // TODO Create a new widget for this list
-                    return Consumer<SeedProvider>(
-                      builder: (context, seedProvider, _) {
-                        if (seedProvider.allSeeds.isEmpty) {
-                          return Center(
-                            child: Text(
-                              'Você ainda não possui sementes. \nComece a registrar',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .labelMedium!
-                                  .copyWith(color: Colors.black),
-                              textAlign: TextAlign.center,
-                            ),
-                          );
-                        } else {
-                          return SingleChildScrollView(
-                            child: ExpansionPanelList.radio(
-                              children: seedProvider.allSeeds
-                                  .map((seed) => ExpansionPanelRadio(
-                                      value: seed.id,
-                                      canTapOnHeader: true,
-                                      headerBuilder: (context, _) => ListTile(
-                                            leading: Icon(
-                                              Icons.nature,
-                                              color: seed.synchronized == 1
-                                                  ? Theme.of(context)
-                                                      .primaryColor
-                                                  : Theme.of(context)
-                                                      .errorColor,
-                                            ),
-                                            title: Text(seed.name),
-                                          ),
-                                      body: Column(children: [
-                                        Row(
-                                          children: [
-                                            const Text('Fabricado em: '),
-                                            Text(seed.manufacturedAt)
-                                          ],
-                                        ),
-                                        Row(
-                                          children: [
-                                            const Text('Válido até: '),
-                                            Text(seed.expiresIn)
-                                          ],
-                                        )
-                                      ])))
-                                  .toList(),
-                            ),
-                          );
-                        }
-                      },
+                    return Expanded(
+                      child: Consumer<SeedProvider>(
+                        builder: (context, seedProvider, _) {
+                          if (seedProvider.allSeeds.isEmpty) {
+                            return _buildNoSeedsWarning();
+                          } else {
+                            return _buildSeedsList(seedProvider);
+                          }
+                        },
+                      ),
                     );
                   }
                 },
@@ -231,6 +87,229 @@ class _SeedsScreenState extends State<SeedsScreen> {
           Navigator.of(context).pushNamed(AddNewSeedScreen.routName);
         },
       ),
+    );
+  }
+
+  List<Widget> _buildAppBarIconButtons() {
+    return [
+      IconButton(
+        icon: const Icon(Icons.sync),
+        onPressed: () async {
+          try {
+            await Provider.of<SeedProvider>(context, listen: false)
+                .synchronize();
+            showSnackBar(context, 'Sementes sincronizadas com sucesso');
+          } catch (error) {
+            showSnackBar(context, error.toString());
+          }
+        },
+      ),
+      IconButton(
+          onPressed: () async {
+            final areThereNonSynchronizedSeeds =
+                await Provider.of<SeedProvider>(context, listen: false)
+                    .areThereAnyNonSynchronized();
+            if (areThereNonSynchronizedSeeds) {
+              final isLoggingOut = await showDialog<bool>(
+                context: context,
+                builder: (_) => AlertDialog(
+                  title: const Text(
+                    'Sementes não sincronizadas',
+                    style: TextStyle(color: Colors.black, fontSize: 24),
+                  ),
+                  content: const Text(
+                      'Se você sair agora, perderá as sementes não sincronizadas'),
+                  actions: [
+                    TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop(true);
+                        },
+                        child: const Text(
+                          'Sair',
+                          style: TextStyle(color: Colors.green),
+                        )),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop(false);
+                        },
+                        child: const Text('Sincronizar',
+                            style: TextStyle(color: Colors.green)))
+                  ],
+                ),
+              );
+
+              if (isLoggingOut != null && isLoggingOut) {
+                await Provider.of<SeedProvider>(context, listen: false).clear();
+                await Provider.of<AuthProvider>(context, listen: false)
+                    .logout();
+              }
+            } else {
+              await Provider.of<AuthProvider>(context, listen: false).logout();
+            }
+          },
+          icon: const Icon(Icons.logout))
+    ];
+  }
+
+  Widget _buildSearchBox() {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: TextField(
+        decoration: InputDecoration(
+            enabledBorder: const OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(8)),
+                borderSide: BorderSide(color: Colors.green)),
+            focusedBorder: const OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(8)),
+                borderSide: BorderSide(color: Colors.green, width: 2.0)),
+            hintText: 'Buscar sementes...',
+            hintStyle: Theme.of(context)
+                .textTheme
+                .labelMedium!
+                .copyWith(color: Colors.black)),
+        onChanged: (query) {
+          Provider.of<SeedProvider>(context, listen: false).searchSeeds(query);
+        },
+      ),
+    );
+  }
+
+  Widget _buildTimeOutExceptionWarning(AsyncSnapshot snapshot) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            snapshot.error.toString(),
+            textAlign: TextAlign.center,
+            style: Theme.of(context)
+                .textTheme
+                .labelMedium!
+                .copyWith(color: Colors.black),
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              setState(() {});
+            },
+            child: Text(
+              'Recarregar',
+              style: Theme.of(context)
+                  .textTheme
+                  .button!
+                  .copyWith(color: Colors.white),
+            ),
+            style: ElevatedButton.styleFrom(
+              primary: Theme.of(context).primaryColor,
+              padding: const EdgeInsets.all(16),
+              fixedSize: Size(MediaQuery.of(context).size.width * 0.7, 60),
+              elevation: 16,
+              shadowColor: Colors.green,
+              shape: const StadiumBorder(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildNoSeedsWarning() {
+    return Center(
+      child: Text(
+        'Não há sementes',
+        style: Theme.of(context)
+            .textTheme
+            .labelMedium!
+            .copyWith(color: Colors.black),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+
+  Widget _buildSeedsList(SeedProvider seedProvider) {
+    return ListView.builder(
+      itemCount: seedProvider.allSeeds.length,
+      itemBuilder: (context, index) {
+        final seed = seedProvider.allSeeds[index];
+        return Card(
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(16),
+            ),
+          ),
+          elevation: 8,
+          child: Container(
+            margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+            padding: const EdgeInsets.all(4),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: 140,
+                      child: Text(
+                        seed.name,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 140,
+                      child: Text(
+                        seed.manufacturer,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        const Text(
+                          'Fabricado em: ',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        Text(DateFormat('dd/MM/yyyy')
+                            .format(seed.manufacturedAt))
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        const Text('Válida até: ',
+                            style: TextStyle(fontWeight: FontWeight.bold)),
+                        Text(DateFormat('dd/MM/yyyy').format(seed.expiresIn))
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        const Text('Registrada em: ',
+                            style: TextStyle(fontWeight: FontWeight.bold)),
+                        Text(DateFormat('dd/MM/yyyy').format(seed.createdAt))
+                      ],
+                    )
+                  ],
+                ),
+                Icon(
+                  Icons.sync,
+                  color: seed.synchronized == 1 ? Colors.green : Colors.red,
+                )
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
