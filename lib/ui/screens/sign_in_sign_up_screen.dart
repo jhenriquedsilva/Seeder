5b4ca6db421bdeecd4d262a2abf18e6fc0c87abb
jhenriquedsilva <josehs555@gmail.com>
2022-06-05 19:00:08 -0300

    feature: improve sign in/up screen

--- lib/ui/screens/sign_in_sign_up_screen.dart
+++ lib/ui/screens/sign_in_sign_up_screen.dart
@@ -9,30 +9,32 @@ class SingInSignUpScreen extends StatefulWidget {
 }
 
 class _SingInSignUpScreenState extends State<SingInSignUpScreen> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
       resizeToAvoidBottomInset: true,
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                margin: const EdgeInsets.all(16),
-                padding: const EdgeInsets.all(8),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('Aegro', style: Theme.of(context).textTheme.headline6)
-                  ],
-                ),
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).requestFocus(FocusNode());
+        },
+        child: SafeArea(
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.all(16),
+                    child: Image.network(
+                      'https://play-lh.googleusercontent.com/3GIcAg0p2xsj-BuSEw2oima33RDarawUoJeWRWb-AIuq0Zxb5XwvckqRpONPSGodtUjh',
+                      width: 200,
+                      height: 200,
+                    ),
+                  ),
+                  const InputForm(),
+                ],
               ),
-              const InputForm(),
-            ],
+            ),
           ),
         ),
       ),
