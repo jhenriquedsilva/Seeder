03e85c568361e4112227a66e1f76b219eb617c1d
jhenriquedsilva <josehs555@gmail.com>
2022-06-06 02:57:36 -0300

    refactor: add user and user id to parameters

--- lib/network/seed_service.dart
+++ lib/network/seed_service.dart
@@ -3,17 +3,13 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:http/http.dart';
-import 'package:seed/database/seeder_database.dart';
 import 'package:seed/exceptions/time_exceeded_exception.dart';
 import 'package:seed/exceptions/unavailable_server.dart';
 
 import '../models/network_seed.dart';
+import '../models/user.dart';
 
 class SeedService {
-  SeedService(SeederDatabase seederDatabase)
-      : _seederDatabase = seederDatabase;
-
-  final SeederDatabase _seederDatabase;
   final baseUrl =
       'https://learning-data-sync-mobile.herokuapp.com/datasync/api/seed';
   final headers = {
@@ -21,14 +17,11 @@ class SeedService {
     'accept': 'application/json',
   };
 
-  Future<List<NetworkSeed>> fetch() async {
-    final userList = await _seederDatabase.getUser();
-    final user = userList[0];
-    final url = Uri.parse('$baseUrl/${user.id}');
-
+  Future<List<NetworkSeed>> fetch(String userId) async {
+    final url = Uri.parse('$baseUrl/$userId');
 
     try {
-      // await Future.delayed(const Duration(seconds: 5,),() => throw TimeExceededException());
+      // final response = await Future.delayed(const Duration(seconds: 5,),() => throw TimeExceededException());
       final response = await get(url).timeout(
         const Duration(seconds: 30),
         onTimeout: () => throw TimeExceededException(),
@@ -52,15 +45,13 @@ class SeedService {
       }
     } on SocketException {
       throw Exception('Você não possui internet');
-
     } catch (error) {
       rethrow;
     }
   }
 
-  Future<void> send(List<NetworkSeed> networkSeeds) async {
+  Future<void> send(User user, List<NetworkSeed> networkSeeds) async {
     final url = Uri.parse(baseUrl);
-    final userList = await _seederDatabase.getUser();
 
     try {
       await Future.forEach<NetworkSeed>(
@@ -68,7 +59,7 @@ class SeedService {
         (networkSeed) async {
           final response = await post(
             url,
-            body: networkSeed.toJson(userList[0].id),
+            body: networkSeed.toJson(user.id),
             headers: headers,
           ).timeout(
             const Duration(seconds: 30),
@@ -77,16 +68,13 @@ class SeedService {
 
           if (response.statusCode >= 400 && response.statusCode < 500) {
             throw Exception('Um erro ocorreu. Tente novamente');
-
           } else if (response.statusCode == 503) {
             throw UnavailableServerException();
-
           } else {
             throw Exception('Erro no servidor. Tente novamente mais tarde');
           }
         },
       );
-
     } on SocketException {
       throw Exception('Você não possui internet');
     } catch (error) {
