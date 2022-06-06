3cfa1d8303ebb94c83ed6e59f0be2bf59bdf13b2
jhenriquedsilva <josehs555@gmail.com>
2022-06-06 08:46:49 -0300

    feature: treat error loading problems

--- lib/ui/screens/sign_in_sign_up_screen.dart
+++ lib/ui/screens/sign_in_sign_up_screen.dart
@@ -26,9 +26,17 @@ class _SingInSignUpScreenState extends State<SingInSignUpScreen> {
                   Container(
                     margin: const EdgeInsets.all(16),
                     child: Image.network(
-                      'https://play-lh.googleusercontent.com/3GIcAg0p2xsj-BuSEw2oima33RDarawUoJeWRWb-AIuq0Zxb5XwvckqRpONPSGodtUjh',
+                      'https://play-lh.googleusercontent.com/'
+                          '3GIcAg0p2xsj-BuSEw2oima33RDarawUoJeWRWb'
+                          '-AIuq0Zxb5XwvckqRpONPSGodtUjh',
                       width: 200,
                       height: 200,
+                      errorBuilder: (context, exception, stackTrace) {
+                        return Text(
+                          'Aegro',
+                          style: Theme.of(context).textTheme.headline6,
+                        );
+                      },
                     ),
                   ),
                   const InputForm(),
