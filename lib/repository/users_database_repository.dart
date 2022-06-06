44d63660ca9da4ab8d8dac5415be9e4e4b3ce198
jhenriquedsilva <josehs555@gmail.com>
2022-06-06 01:30:05 -0300

    refactor: create users database repository

---
+++ lib/repository/users_database_repository.dart
@@ -0,0 +1,32 @@
+import 'package:seed/database/database_provider.dart';
+import 'package:seed/database/user_dao.dart';
+import 'package:seed/models/user.dart';
+import 'package:seed/repository/users_repository.dart';
+
+class UsersDatabaseRepository implements UsersRepository {
+  UsersDatabaseRepository(this.databaseProvider);
+
+  final dao = UserDao();
+  @override
+  DatabaseProvider databaseProvider;
+
+  @override
+  Future<void> clear() async {
+    final db = await databaseProvider.db();
+    await db.delete(dao.tableName);
+  }
+
+  @override
+  Future<List<User>> getUsers() async {
+    final db = await databaseProvider.db();
+    List<Map<String, dynamic>> maps = await db.query(dao.tableName);
+    return dao.fromList(maps);
+  }
+
+  @override
+  Future<void> insert(User user) async {
+    final db = await databaseProvider.db();
+    await db.insert(dao.tableName, dao.toMap(user));
+  }
+
+}
