9884c0257828a70334f47e108d4f318ea92d634b
jhenriquedsilva <josehs555@gmail.com>
2022-06-09 08:15:31 -0300

    refactor: update auth service to make use of http service

--- lib/network/auth_service.dart
+++ lib/network/auth_service.dart
@@ -1,114 +1,67 @@
 import 'dart:convert';
-import 'dart:io';
-
 import 'package:http/http.dart';
-import 'package:seed/exceptions/no_internet_exception.dart';
-import 'package:seed/exceptions/server_exception.dart';
+import 'package:seed/network/http_sevice.dart';
 import 'package:uuid/uuid.dart';
 
-import '../exceptions/email_already_exist_exception.dart';
-import '../exceptions/email_does_not_exist_exception.dart';
-import '../exceptions/email_not_valid_exception.dart';
-import '../exceptions/local_exception.dart';
 import '../exceptions/time_exceeded_exception.dart';
-import '../exceptions/unavailable_server.dart';
 import '../models/user.dart';
 
-class AuthService {
-  final _baseUrl = 'https://learning-data-sync-mobile.herokuapp.com/datasync/api';
-  final headers = {
-    'content-type': 'application/json',
-    'accept': 'application/json',
-  };
-
-  Future<User> login(String email) async  {
-
+class AuthService extends HttpService {
+  Future<User> login(String email) async {
     try {
+      const endpoint = '/user/auth';
+      await verifyInternetConnection();
 
-      final url = Uri.parse('$_baseUrl/user/auth');
-      final response = await post(
-          url,
-          body: json.encode(
-            {'email': email},
-          ),
-          headers: headers
-      ).timeout(
-        const Duration(seconds: 30),
+      final url = Uri.parse(baseUrl + endpoint);
+      final response = await post(url,
+              body: json.encode(
+                {'email': email},
+              ),
+              headers: headers)
+          .timeout(
+        timeoutDuration,
         onTimeout: () => throw TimeExceededException(),
       );
 
-      if (response.statusCode >= 200 && response.statusCode < 300){
+      if (response.statusCode >= 200 && response.statusCode < 300) {
         return User.fromMap(json.decode(response.body));
-
-      } else if (response.statusCode == 404) {
-        throw EmailDoesNotExistException();
-
-      } else if (response.statusCode >= 400 && response.statusCode < 500) {
-        throw LocalException();
-
-      } else if (response.statusCode == 503) {
-        throw UnavailableServerException();
-
       } else {
-        throw ServerException();
+        throw createAppropriateException(response.statusCode);
       }
-
-    } on SocketException {
-      throw NoInternetException();
-
     } catch (error) {
       rethrow;
     }
-
-}
+  }
 
   Future<User> signup(String fullName, String email) async {
-
-    final url = Uri.parse('$_baseUrl/user');
-    final id = const Uuid().v4().toString();
-    final user = {
-      'id': id,
-      'fullName': fullName,
-      'email': email,
-    };
-
     try {
+      const endpoint = '/user';
+      await verifyInternetConnection();
+
+      final url = Uri.parse(baseUrl + endpoint);
+      final id = const Uuid().v4().toString();
+      final user = {
+        'id': id,
+        'fullName': fullName,
+        'email': email,
+      };
 
       final response = await post(
         url,
         headers: headers,
         body: json.encode(user),
       ).timeout(
-        const Duration(seconds: 30),
+        timeoutDuration,
         onTimeout: () => throw TimeExceededException(),
       );
-      print(response.body);
 
       if (response.statusCode >= 200 && response.statusCode < 300) {
         return User.fromMap(user);
-
-      } else if (response.statusCode == 400) {
-        throw EmailNotValidException();
-
-      } else if (response.statusCode == 409) {
-        throw EmailAlreadyExistException();
-
-      } else if (response.statusCode >= 400 && response.statusCode < 500) {
-        throw LocalException();
-
-      } else if (response.statusCode == 503) {
-        throw UnavailableServerException();
-
       } else {
-        throw ServerException();
+        throw createAppropriateException(response.statusCode);
       }
-
-    } on SocketException {
-      throw NoInternetException();
-
     } catch (error) {
       rethrow;
     }
-
   }
 }
