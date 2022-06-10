df265525aaaa5ef7961f9e6cb968ff8d2364dd7a
jhenriquedsilva <josehs555@gmail.com>
2022-06-10 16:43:26 -0300

    refactor: replace by a specific style

--- lib/ui/widgets/custom_text_button.dart
+++ lib/ui/widgets/custom_text_button.dart
@@ -12,13 +12,20 @@ class CustomTextButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return TextButton(
-        child: Text(text, style: Theme.of(context).textTheme.button),
-        onPressed: () {
-          navigationHandler();
-        },
-        style: TextButton.styleFrom(
-          padding: const EdgeInsets.symmetric(vertical: 16),
+      child: Text(
+        text,
+        style: const TextStyle(
+          color: Colors.black,
+          fontSize: 16,
+          fontWeight: FontWeight.w900,
         ),
-      );
+      ),
+      onPressed: () {
+        navigationHandler();
+      },
+      style: TextButton.styleFrom(
+        padding: const EdgeInsets.symmetric(vertical: 16),
+      ),
+    );
   }
 }
